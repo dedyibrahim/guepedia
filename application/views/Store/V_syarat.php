d946bdebffc93e88199805c1fe7f9cafd93d9e78
dedyibrahim <dedyibrahym23@gmail.com>
2018-09-25 16:19:10 +0700

    OKE

--- application/views/Store/V_syarat.php
+++ application/views/Store/V_syarat.php
@@ -1,13 +1,12 @@
 <div class="container" style="margin-top:6%; ">
     <h4 align="center"><span class="fa fa-list-ul fa-3x"> </span> <br> Syarat & Ketentuan</h4><hr>
-    <br>
-    <br>
-    <br>
-    <br><br>
-    <br>
-    <br>
-    <br><br>
-    <br>
-    
+   
+  <br>Naskah yang dikirim ke email dengan persyaratan berikut:
+  <br>•&nbsp;&nbsp;&nbsp; minimal 50 halaman A4, maksimal bebas tidak dibatasi, dalam bentuk word 
+  <br>•&nbsp;&nbsp;&nbsp; font Bookman Old Style 10 pt
+  <br>•&nbsp;&nbsp;&nbsp; spasi 1.15
+  <br>•&nbsp;&nbsp;&nbsp; margin 2 cm<br>•&nbsp;&nbsp;&nbsp; &nbsp;setiap bab baru harus di mulai di halaman ganjil
+  <br>•&nbsp;&nbsp;&nbsp; harus ada untuk semua jenis naskah : kata pengantar, daftar isi, tentang penulis, dan sinopsis&nbsp; 
+  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; sekitar 250 kata ( jika bagian ini tidak ada, naskah tidak akan di proses )<br>•&nbsp;&nbsp;&nbsp; daftar pustaka bila buku umum atau pengetahuan<br>•&nbsp;&nbsp;&nbsp; tidak mengandung SARA, pornografi, LGBT, plagiat, dan hal-hal yang menimbulkan kontroversi <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; publik<br>•&nbsp;&nbsp;&nbsp; jika ingin membuat cover sendiri silahkan dibuat di photoshop dengan ukuran A4 dan resolusi <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; gambar minimal 300 pixel dan kirim ke email dengan format psd, bukan jpg/jpeg/png<br>ingat ya kak, <br>1. disain, layout editing, guepedia yang buatkan<br>2. Cover guepedia yang buatkan<br>3. ISBN dapat gratis<br>4. Promosi guepedia yang kerjakan..<br>penulis tinggal duduk santai, tiap bulan dapat royalty 15 %, asiik kan...<br><br>Proses penerbitan satu buku kurang lebih 10 hari kerja<br>Buku nantinya akan dijual via online di market place seluruh indonesia, media sosial, dan google book<br>Sistem laporan penjulan kita sudah real time,bisa di lihat di akun penulis, laporan penjualan setiap hari bisa di lihat secara online..<br>Royalti yang didapatkan untuk penulis 15% dari harga buku di setiap penjualan<br>Royalti bisa diambil sebulan sekali, batas maksimal proses pengambilan royalti di tanggal 5 setiap bulannya, dan akan dicairkan di tanggal 10 setiap bulannya<br>Jika ada pertanyaan lain silahkan tanyakan via email di <a href="mailto:guepedia@gmail.com" target="_blank">guepedia@gmail.com</a> atau WA di 081287602508<br>NB:<br>1.&nbsp;&nbsp;&nbsp; Karena ini program gratis dan semuanya sudah gratis, jika penulis ingin bukunya bisa beli dengan diskon khusus.<br>2.&nbsp;&nbsp;&nbsp; Naskah yang masuk&nbsp; ada seleksi.&nbsp; <br>3.&nbsp;&nbsp;&nbsp; Naskah semua genre di terima, termasuk puisi dan cerpen <br>4.&nbsp;&nbsp;&nbsp; Guepedia berhak untuk menolak naskah yang akan di terbitkan dengan atau tanpa alasan penolakannya, biasanya kita terima kok..kita hargai semua jenis naskah<br>5.&nbsp;&nbsp;&nbsp; Penulis boleh mengirim / menerbitkan lebih dari satu naskah, tidak di batasi<br>6.&nbsp;&nbsp;&nbsp; Naskah yang sudah pernah di terbitkan di penerbit lain, tidak bisa kami terbitkan, kecuali di sertai surat pencabutan naskah.<br>7.&nbsp;&nbsp;&nbsp; Naskah yang pernah di posting/upload di sosmed, aplikasi menulis, spt wattpad, dll..silahkan di kirim dan akan kami terbitkan .<br>8.&nbsp;&nbsp;&nbsp; Kami berhak mencabut naskah yang sudah di terbitkan jika di kemudian hari di temukan pelanggaran yang tidak sesuai dengan ketentuan guepedia, misalkan : plagiat, SARA, melanggar hukum,dll<br><br>Jika ada pertanyaan lain silahkan tanyakan via email di <a href="mailto:guepedia@gmail.com" target="_blank">guepedia@gmail.com</a> atau WA di 081287602508<br><br>Terima kasih<br>Guepedia<br><br>
     
 </div>
