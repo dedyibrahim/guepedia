cbfd16e082efabf0742b2bf802926458cf04d907
dedyibrahim <dedyibrahym23@gmail.com>
2018-10-02 16:52:29 +0700

    ASDasdsdf

--- application/controllers/Not_found.php
+++ application/controllers/Not_found.php
@@ -2,9 +2,9 @@
 class Not_found extends CI_Controller{
     function index(){
      $this->load->view('Umum/V_header_toko');   
-     $this->load->view('store/V_header_toko');   
+     $this->load->view('Store/V_header_toko');   
      $this->load->view('not_found.php');  
-     $this->load->view('umum/V_footer_toko');
+     $this->load->view('Umum/V_footer_toko');
     }
     
 }
